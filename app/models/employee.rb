64957ece8546cd909be955fda5eb00f3559acc3f
MarcoLc94 <marcoantoniolopezcastro@hotmail.com>
2023-07-20 10:40:59 -0600

    Views

---
+++ app/models/employee.rb
@@ -0,0 +1,7 @@
+class Employee < ApplicationRecord
+  belongs_to :department, counter_cache: true
+  belongs_to :manager, class_name: "Employee", optional: true
+  has_many :reports, class_name: "Employee", foreign_key: "manager_id", dependent: :destroy,
+                     inverse_of: "manager"
+  has_one_attached :avatar
+end
